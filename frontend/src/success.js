9c68800f52c015b47bd70ecd5b9caccf76288133
Techo Anant <anantk.singh9876@gmail.com>
2025-07-27 18:30:29 -0400

    login route fixed

--- frontend/src/success.js
+++ frontend/src/success.js
@@ -4,7 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 const success = () => {
   return (
     <div>
-        <div className='d-flex align-item-center'>
+        <div className='d-flex w-100 h-100 justify-content-center align-items-center'>
             <h1>Hi there, you have successfully logged in!</h1>
         </div>
     </div>
